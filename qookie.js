82c63c81acebd587b7bd867847fb03f7d6f1bd1d
Qrzysio <krzysiek@chojnowski.us>
2016-05-05 14:59:30 +0800

    2016.05.05

--- qookie.js
+++ qookie.js
@@ -1,50 +1,44 @@
 $.fn.extend({
-        cookiesEU: function(options) {
-        	var host = $(location).attr('protocol')+'//'+$(location).attr('hostname');
-            var defaults = {
-                text: 'W ramach witryny stosujemy pliki cookies celem świadczenia usług na najwyższym poziomie. <a href="'+host+'/polityka-cookies/" title="Polityka cookies">Dowiedz się więcej</a>.',
-                close: '',
-                parent: $('body'),
-                show_close: true,
-                cookie_name: 'cookieEuAgree_js',
-                auto_accept: true,
-                domain: '',
-                test: false
-            };
-            var o = $.extend(defaults, options);
-	        $('#cookieEU').remove();
-            var box = $('<div id="cookieEU"><div class="inner"><div class="text">'+o.text+'</div></div></div>');
-            if (o.show_close == true) box.find('div.text').prepend('<a href="#" class="close">'+o.close+'</a>');
-            if (readCookie(o.cookie_name) == null) {
-                o.parent.prepend(box);                
-                if (!o.test && o.auto_accept) createCookie(o.cookie_name, 1, 365, o.domain)
-            };
-            box.find('a.close').click(function(e) {
-                e.preventDefault();
-                if (!o.test && !o.auto_accept) createCookie(o.cookie_name, 1, 365, o.domain);
-                box.slideUp(350);                
-            });
+  Qookie: function(options) {
+    var host = $(location).attr('protocol')+'//'+$(location).attr('hostname');
+    var defaults = {
+      text: 'W ramach witryny stosujemy pliki cookies celem świadczenia usług na najwyższym poziomie. <a href="'+host+'/polityka-cookies/" title="Polityka cookies">Dowiedz się więcej</a>.',
+      parent: $('body'),
+      cookie_name: 'QookieEuAgree_js',
+      auto_accept: true,
+      test: false
+    };
+    var o = $.extend(defaults, options);
+    $('#Qookie').remove();
+    var box = $('<div id="Qookie"><div class="inner"><div class="text"><span class="close"></span>'+o.text+'</div></div></div>');
+    if (readCookie(o.cookie_name) == null) {
+      o.parent.prepend(box);                
+      if (!o.test && o.auto_accept) createCookie(o.cookie_name, 1, 365)
+    };
+    box.find('span').click(function(e) {
+      if (!o.test && !o.auto_accept) createCookie(o.cookie_name, 1, 365);
+      box.slideUp(350);                
+    });
 
-            function createCookie(name, value, days, domain) {
-                if (days) {
-                    var date = new Date();
-                    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                    var expires = "; expires=" + date.toGMTString()
-                } else var expires = "";
-                var cookie = name + "=" + value + expires + "; path=/";
-                if (domain.length > 0) cookie = cookie + "; domain=" + domain;
-                document.cookie = cookie
-            };
+    function createCookie(name, value, days) {
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString()
+      } else var expires = "";
+      var cookie = name + "=" + value + expires + "; path=/";
+      document.cookie = cookie
+    };
 
-            function readCookie(name) {
-                var nameEQ = name + "=";
-                var ca = document.cookie.split(';');
-                for (var i = 0; i < ca.length; i++) {
-                    var c = ca[i];
-                    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
-                }
-                return null
-            }
-        }
-    })
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+      }
+    return null
+    }
+  }
+})
